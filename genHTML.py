d3d476f41eaf8417c65a94b35601c7095de1e002
Toni Zeidler <toni.z@gmx.de>
2024-02-24 10:26:37 +0100

    ok

--- genHTML.py
+++ genHTML.py
@@ -6,7 +6,7 @@ def generate_html(video_filename, content_lines, output_filename='content.html')
 <html lang="de">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
+    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
